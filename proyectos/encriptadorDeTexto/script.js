e48e9b5adc9d8834973e8d88308c7e604fd12888
Hernán Rodrigo Bustamante <123601168+Rodrigo5b@users.noreply.github.com>
2023-04-05 19:49:36 -0300

    1.1
    Me falta JavaScript y responsividad en toda la página,

---
+++ proyectos/encriptadorDeTexto/script.js
@@ -0,0 +1,10 @@
+const texto = document.getElementById("texto");
+const salida = document.getElementById("salida")
+const encriptar = document.getElementById("encriptar")
+const desencriptar = document.getElementById("desencriptar")
+const copy = document.getElementById("copy")
+
+encriptar.addEventListener('click',() =>{
+    console.log(encriptar)
+    
+})
