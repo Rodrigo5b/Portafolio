6dfe435d07e132a7fc094403101abd65bdf5e5ec
Hernán Rodrigo Bustamante <123601168+Rodrigo5b@users.noreply.github.com>
2023-04-12 23:51:51 -0300

    Add files via upload
    13/04
    Arregle un poco la parte responsiva de la pagina, me falta Javascript, los proyectos y el menú de navegación.

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+// function copy(element){
+//     var temp=document.createElement("input");
+
+//     document.getElementsByTagName("body")[0].appendChild(temp);
+//     temp.value = element.innerHTML;
+//     temp.select();
+
+
+//     document.execCommand
+//     document.getElementsByTagName('body').removeChild(temp)
+// }
+
+navigator.clipboard.readText().then(
+    clipText => document.querySelector(".editor").innerText += clipText);
