a8ad2040533e0b613a656df1d99cf68c2b4a9f14
Hernán Rodrigo Bustamante <123601168+Rodrigo5b@users.noreply.github.com>
2023-05-03 03:04:39 -0300

    Add files via upload

---
+++ assets/components/script.js
@@ -0,0 +1,6 @@
+// const pasaje = document.getElementsByClassName('pasaje__item')
+// const siguiente = document.getElementsByClassName('siguiente')
+// const anterior = document.getElementsByClassName('anterior')
+const boton = document.getElementById('boton');
+
+console.log(boton);
